6a382fb34c543e2135efcb71d4a3695ee5f8ab33
Neylson Crepalde <neylsoncrepalde@gmail.com>
2024-01-29 07:36:11 -0300

    feat/add .gitignore and get_imdb_data.py

---
+++ Chapter 5/get_imdb_data.py
@@ -0,0 +1,20 @@
+import os
+import requests
+
+urls_dict = {
+    "basics.tsv.gz": "https://datasets.imdbws.com/title.basics.tsv.gz",
+    "crew.tsv.gz": "https://datasets.imdbws.com/title.crew.tsv.gz",
+    "principals.tsv.gz": "https://datasets.imdbws.com/title.principals.tsv.gz",
+    "ratings.tsv.gz": "https://datasets.imdbws.com/title.ratings.tsv.gz"
+}
+
+def get_imdb_data(urls):
+    for title, url in urls.items():
+        response = requests.get(url, stream=True)
+        with open(f"data/imdb/{title}", mode="wb") as file:
+            file.write(response.content)
+    return True
+
+
+os.makedirs('data/imdb', exist_ok=True)
+get_imdb_data(urls_dict)
