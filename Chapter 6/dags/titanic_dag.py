ce992536bf5b734ef3d7efe3f1f18dc38f427be9
Neylson Crepalde <neylsoncrepalde@gmail.com>
2024-02-12 10:55:26 -0300

    feat/complete Titanic DAG.

--- Chapter 6/dags/titanic_dag.py
+++ Chapter 6/dags/titanic_dag.py
@@ -1,11 +1,13 @@
 from airflow.decorators import task, dag
 from airflow.operators.dummy import DummyOperator
-from airflow.models import Variable
+from airflow.operators.bash import BashOperator
 from datetime import datetime
+import requests
+import pandas as pd
 
 default_args = {
     'owner': 'Ney',
-    'start_date': datetime(2022, 4, 2)
+    'start_date': datetime(2024, 2, 12)
 }
 
 @dag(
@@ -17,12 +19,50 @@ default_args = {
 )
 def titanic_processing():
 
+    # Task Definition
     start = DummyOperator(task_id='start')
 
     @task
     def first_task():
         print("And so, it begins!")
 
-    ### Some more tasks here...
+    @task
+    def download_data():
+        destination = "/tmp/titanic.csv"
+        response = requests.get(
+            "https://raw.githubusercontent.com/neylsoncrepalde/titanic_data_with_semicolon/main/titanic.csv", 
+            stream=True
+        )
+        with open(destination, mode="wb") as file:
+            file.write(response.content)
+        return destination
+
+    @task
+    def analyze_survivors(source):
+        df = pd.read_csv(source, sep=";")
+        res = df.loc[df.Survived == 1, "Survived"].sum()
+        print(res)
+
+    @task
+    def survivors_sex(source):
+        df = pd.read_csv(source, sep=";")
+        res = df.loc[df.Survived == 1, ["Survived", "Sex"]].groupby("Sex").count()
+        print(res)
+
+    last = BashOperator(
+        task_id="last_task",
+        bash_command='echo "This is the last task performed with Bash."',
+    )
+
+    end = DummyOperator(task_id='end')
+
+    # Orchestration
+    first = first_task()
+    downloaded = download_data()
+    start >> first >> downloaded
+    surv_count = analyze_survivors(downloaded)
+    surv_sex = survivors_sex(downloaded)
+
+    [surv_count, surv_sex] >> last >> end
 
 execution = titanic_processing()
