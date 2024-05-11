1dbe8d8a3fcca1de028cca77a46303c8c31aefb8
Neylson Crepalde <neylsoncrepalde@gmail.com>
2024-05-11 19:37:31 -0300

    fix/boto3 s3 client

--- Chapter 10/batch/dags/imdb_dag.py
+++ Chapter 10/batch/dags/imdb_dag.py
@@ -11,7 +11,10 @@ import boto3
 aws_access_key_id = Variable.get("aws_access_key_id")
 aws_secret_access_key = Variable.get("aws_secret_access_key")
 
-s3 = boto3.client('s3', aws_access_key_id, aws_secret_access_key)
+s3 = boto3.client('s3', 
+    aws_access_key_id=aws_access_key_id, 
+    aws_secret_access_key=aws_secret_access_key
+)
 
 default_args = {
     'owner': 'Ney',
