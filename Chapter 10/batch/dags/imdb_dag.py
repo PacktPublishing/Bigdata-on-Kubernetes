070e8ed730dbc7d41dafef3a71191bbf34295310
Neylson Crepalde <neylsoncrepalde@gmail.com>
2024-05-11 18:32:24 -0300

    feat/imdb batch dag

---
+++ Chapter 10/batch/dags/imdb_dag.py
@@ -0,0 +1,91 @@
+from airflow.decorators import task, dag
+from airflow.utils.task_group import TaskGroup
+from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
+from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
+from airflow.providers.amazon.aws.operators.glue_crawler import GlueCrawlerOperator
+from datetime import datetime
+import requests
+import boto3
+
+s3 = boto3.client('s3')
+
+default_args = {
+    'owner': 'Ney',
+    'start_date': datetime(2024, 5, 10)
+}
+
+@dag(
+        default_args=default_args, 
+        schedule_interval="@once", 
+        description="IMDB Dag", 
+        catchup=False, 
+        tags=['IMDB']
+)
+def IMDB_batch():
+
+    @task
+    def data_acquisition():
+        urls_dict = {
+            "names.tsv.gz": "https://datasets.imdbws.com/name.basics.tsv.gz",
+            "basics.tsv.gz": "https://datasets.imdbws.com/title.basics.tsv.gz",
+            "crew.tsv.gz": "https://datasets.imdbws.com/title.crew.tsv.gz",
+            "principals.tsv.gz": "https://datasets.imdbws.com/title.principals.tsv.gz",
+            "ratings.tsv.gz": "https://datasets.imdbws.com/title.ratings.tsv.gz"
+        }
+
+        for title, url in urls_dict.items():
+            response = requests.get(url, stream=True)
+            with open(f"/tmp/{title}", mode="wb") as file:
+                file.write(response.content)
+            s3.upload_file(f"/tmp/{title}", "bdok-539445819060", f"landing/imdb/{title}")
+        
+        return True
+    
+
+    with TaskGroup("tsvs_to_parquet") as tsv_parquet:
+        tsvs_to_parquet = SparkKubernetesOperator(
+            task_id="tsvs_to_parquet",
+            namespace="airflow",
+            application_file="spark_imdb_tsv_parquet.yaml",
+            kubernetes_conn_id="kubernetes_default",
+            do_xcom_push=True
+        )
+        tsvs_to_parquet_sensor = SparkKubernetesSensor(
+            task_id="tsvs_to_parquet_sensor",
+            namespace="airflow",
+            application_name="{{ task_instance.xcom_pull(task_ids='tsvs_to_parquet')['metadata']['name'] }}",
+            kubernetes_conn_id="kubernetes_default"
+        )
+        tsvs_to_parquet >> tsvs_to_parquet_sensor
+    
+
+    with TaskGroup('Transformations') as transformations:
+        consolidated_table = SparkKubernetesOperator(
+            task_id='consolidated_table',
+            namespace="airflow",
+            application_file="spark_imdb_consolidated_table.yaml"
+            kubernetes_conn_id="kubernetes_default",
+            do_xcom_push=True
+        )
+        consolidated_table_sensor = SparkKubernetesSensor(
+            task_id='consolidated_table_sensor',
+            namespace="airflow",
+            application_name="{{ task_instance.xcom_pull(task_ids='consolidated_table')['metadata']['name'] }}",
+            kubernetes_conn_id="kubernetes_default"
+        )
+        consolidated_table >> consolidated_table_sensor
+
+
+    glue_crawler_consolidated = GlueCrawlerOperator(
+        task_id='glue_crawler_consolidated',
+        crawler_name="imdb_consolidated_crawler",
+        aws_conn_id='aws_conn',
+        wait_for_completion=True
+    )
+
+    
+    # Orchestration
+    da = data_acquisition() 
+    da >> tsv_parquet >> transformations >> glue_crawler_consolidated
+
+execution = IMDB_batch()
